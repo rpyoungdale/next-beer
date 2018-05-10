5cb04115a2f22a7f1318654f01d89f064d2dbad6
rpyoungdale <31086091+rpyoungdale@users.noreply.github.com>
2018-05-10 19:19:56 -0400

    Revert "refactored and added search"

--- next-beer-backend/app/serializers/api/v1/comment_serializer.rb
+++ next-beer-backend/app/serializers/api/v1/comment_serializer.rb
@@ -1,4 +1,3 @@
 class Api::V1::CommentSerializer < ActiveModel::Serializer
-  belongs_to :beer
   attributes :content, :beer_id
 end
--- next-beer-backend/app/serializers/beer_serializer.rb
+++ next-beer-backend/app/serializers/beer_serializer.rb
@@ -1,5 +1,4 @@
 class BeerSerializer < ActiveModel::Serializer
   has_many :restaurants
-  has_many :comments
-  attributes :id, :image, :name, :beer_type, :abv, :ibu, :like_count, :dislike_count, :restaurants, :comments
+  attributes :id, :image, :name, :beer_type, :abv, :ibu, :like_count, :dislike_count, :restaurants
 end
